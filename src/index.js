e96a5afba8e45472cd8e8a7312f125599ae960cb
Yakushyn Mark <yakushyn.mark@gmail.com>
2023-11-14 16:41:53 +0100

    done

--- src/index.js
+++ src/index.js
@@ -1,4 +1,10 @@
 import { fetchBreeds, fetchCatByBreed } from './API/cat-api';
+import SlimSelect from 'slim-select';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+Notify.init({
+  position: 'center-center',
+});
 
 const refs = {
   select: document.querySelector('.breed-select'),
@@ -7,12 +13,40 @@ const refs = {
   error: document.querySelector('.error'),
 };
 
+const fetchCatById = data => {
+  const { value } = data[0];
+  console.log(value);
+
+  refs.catInfo.innerHTML = '';
+
+  refs.loader.classList.remove('hidden');
+
+  fetchCatByBreed(value)
+    .then(renderCat)
+    .catch(err => {
+      refs.loader.classList.add('hidden');
+      Notify.failure('Oops! Something went wrong! Try reloading the page!');
+    });
+};
+
 const renderSelect = ({ data }) => {
+  console.log(data);
   const markup = data
     .map(item => `<option value=${item.id}>${item.name}</option>`)
     .join('');
 
   refs.select.insertAdjacentHTML('beforeend', markup);
+
+  new SlimSelect({
+    select: refs.select,
+    settings: {
+      placeholderText: 'Select breed',
+    },
+    events: {
+      afterChange: fetchCatById,
+    },
+  });
+
   refs.loader.classList.add('hidden');
   refs.select.classList.remove('hidden');
 };
@@ -21,7 +55,7 @@ fetchBreeds()
   .then(renderSelect)
   .catch(err => {
     refs.loader.classList.add('hidden');
-    refs.error.classList.remove('hidden');
+    Notify.failure('Oops! Something went wrong! Try reloading the page!');
   });
 
 const renderCat = ({ data }) => {
@@ -36,23 +70,5 @@ const renderCat = ({ data }) => {
 /><div><h2 class="title">${cat.name}</h2><p class="description">${cat.description}</p><h3>Temperament</h3><p class="temperament">${cat.temperament}</p></div>`;
 
   refs.loader.classList.add('hidden');
-
   refs.catInfo.insertAdjacentHTML('beforeend', markup);
 };
-
-const fetchCatById = event => {
-  const catId = event.currentTarget.value;
-
-  refs.catInfo.innerHTML = '';
-
-  refs.loader.classList.remove('hidden');
-
-  fetchCatByBreed(catId)
-    .then(renderCat)
-    .catch(err => {
-      refs.loader.classList.add('hidden');
-      refs.error.classList.remove('hidden');
-    });
-};
-
-refs.select.addEventListener('change', fetchCatById);
